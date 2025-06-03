68057ed7840cf72bfacfc5274af3091921025466
khadija <khadijabhamla@gmail.com>
2025-06-03 17:13:14 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,12 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbcontext>(opt =>
 {
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));  
-}
-);
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("http://localhost:3000","https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
